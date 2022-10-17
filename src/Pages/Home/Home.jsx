3ee7a642bd4656895580d2e38c7ae9beb10c962b
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-17 23:55:44 +0530

    Description added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,38 @@
 import React,{useEffect,useState} from 'react';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import {Link} from 'react-router-dom';
+import './Home.css';
 export default function Home(){
 
     let [popularMovies,setPopularMovies]=useState([]);
     useEffect(()=>{
         let cont=fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US").then(res=> res.json());
         cont.then(data=> setPopularMovies(data.results));
-        console.log(popularMovies);
     },[])
 
     return(
-        <div>
-            Home Page
+        <div className='poster'> 
+          <Carousel showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}>
+            {
+                popularMovies.map(movie =>(
+                   <Link to={`/movie/${movie.id}`}>
+                    <div className='posterImage'>
+                       <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="" />
+                    </div>
+                    <div className='posterImage_desciption'>
+                        <div className='movie_title'>{movie ? movie.original_title: ""}</div>
+                        <div className='movie_date'>Released on: {movie ? movie.release_date: ""}</div>
+                        <div className='movie_rating'>Rating: {movie ? movie.vote_average:"" }<span><i class="fa fa-star" aria-hidden="true"/></span></div>
+                    </div>
+                   </Link>
+                ))
+            }
+          </Carousel>
         </div>
     );
 }
