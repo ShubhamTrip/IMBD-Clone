cd83b3333b1019deeebc405530a941809d52c035
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-18 22:59:38 +0530

    added search bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Header.css';
 import {Link} from 'react-router-dom';
+import SearchDash from '../SearchDash/SearchDash';
 export default function Header(){
       
      return(
         <div className='header'>
-            <div className='headerleft'>
+            <div className='headerLeft'>
             <Link to='/' style={{textDecoration:'none'}}><img className='header__icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt="imbd logo" /></Link>
             <Link to='/movies/popular' style={{textDecoration: 'none'}}><span>Popular</span></Link>
             <Link to='/movies/top_rated' style={{textDecoration:'none'}}><span>Top Rated</span></Link>
             <Link to='/movies/upcoming' style={{textDecoration:'none'}}><span>Upcoming</span></Link>
-        </div>
+           </div>
+           <div className='headerRight'>
+              <input type="text" placeholder='Search for the movie by name' />
+              <button >Search</button>
+           </div>
+           <SearchDash/>
         </div>
      );
 
---
+++ src/Components/SearchDash/SearchDash.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './SearchDash.css';
+export default function SearchDash(){
+    return (
+        <div className='SearchDash'></div>
+    );
+}
