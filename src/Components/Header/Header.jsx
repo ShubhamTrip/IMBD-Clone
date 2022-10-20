436f7547b001f139c95f352714170a93b79065c3
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-20 12:12:14 +0530

    search functionality added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,45 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './Header.css';
 import {Link} from 'react-router-dom';
 import SearchDash from '../SearchDash/SearchDash';
 export default function Header(){
      let [sd,setsd]=useState('none');
+     let [dataMov,setDataMov]=useState([]);
+     useEffect(() => {
+      getData();
+     }, []);
+
+     function getData(){
+        let arr=[];
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+        .then(prop => prop.json()).then(data=>{
+
+         for(let i=0;i<20;i++){
+            arr.push(data.results[i].original_title);
+         }
+         setDataMov(arr);
+        });
+
+        
+     }
+      
+     let search;
+     function SearchFunc(e){
+       search = e.target.value;
+       let arr = dataMov.filter(func);
+
+       function func(val){
+         for(let i=0;i<search.length;i++){
+             if(search[i]!=val[i]){
+                  return;
+             }
+         }
+         return val;
+       }
+       
+       
+     }
+
      return(
         <div className='header'>
             <div className='headerLeft'>
@@ -13,7 +49,7 @@ export default function Header(){
             <Link to='/movies/upcoming' style={{textDecoration:'none'}}><span>Upcoming</span></Link>
            </div>
            <div className='headerRight'>
-              <input type="text" placeholder='Search for the movie by name' onFocus={()=>{setsd('block')}} onBlur={()=>{setsd('none')}}/>
+              <input type="text" placeholder='Search for the movie by name' onChange={(e)=>{SearchFunc(e)}} onFocus={()=>{setsd('block')}} onBlur={()=>{setsd('none')}}/>
               <button >Search</button>
            </div>
            <SearchDash style={{display:sd}}/>
